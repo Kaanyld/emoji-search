39a699b3ad2c94afee322eebdb6c18f1df6c552b
Kaan <kaanyldrm98@gmail.com>
2023-04-15 23:32:52 +0300

    last commit

--- emoji-search-master/src/test/Emoji.test.js
+++ emoji-search-master/src/test/Emoji.test.js
@@ -7,27 +7,27 @@ import "@testing-library/jest-dom";
 
 
 describe('app tests', () => {
-    beforeEach(() => render(<App />));
+    beforeEach(() => render(<App />));  
   
     test('header', () => {
-      const headerTitle = screen.getByText('Emoji Search');
+      const headerTitle = screen.getByText('Emoji Search'); //Başlık kısmının başarılı şekilde render edildiğini kontrol eden test
       expect(headerTitle).toBeInTheDocument;
     });
   
     test('listed', () => {
-      const items = screen.getAllByText('Click to copy emoji');
+      const items = screen.getAllByText('Click to copy emoji');  // Uygulama ilk açıldığında emoji listesinin başarılı şekilde render edilidiğini kontrol eden test
       expect(items.length).toEqual(20);
     });
   
-    test('filter', () => {
-      const emoji = '100';
-      const input = screen.getByRole('textbox');
+    test('filter', () => {  //filtreleme yapıldığında , listenin uygun şekilde yeniden render edildiğini kontrol eden test
+      const emoji = '100';  
+      const input = screen.getByRole('textbox'); 
       userEvent.type(input, emoji);
       expect(screen.getByText(emoji)).toBeInTheDocument;
     });
   
     test('copy', () => {
-      const copytext = screen.getAllByText('Click to copy emoji');
+      const copytext = screen.getAllByText('Click to copy emoji'); // emojiye tıklandığında kopyalandığını kontrol eden test
       userEvent.click(copytext);
       const input = screen.getByRole('textbox');
       userEvent.paste(input, copytext)
